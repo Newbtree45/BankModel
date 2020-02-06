da67835c264bc8401c43ed7faa577196cb577b54
newberts <newberts@MCD210-PC-12.uccc.uc.edu>
2020-02-06 11:50:29 -0500

    Objects added to Main class

--- InClass_2020_02_06/src/main/Main.java
+++ InClass_2020_02_06/src/main/Main.java
@@ -1,9 +1,18 @@
 package main;
 
+import bank.Bank;
+import bank.BrickAndMortar;
+import bank.CafeBranch;
+import bank.Online;
+
 public class Main {
 
 	public static void main(String[] args) {
-
+		//Declare and instantiate a Bank, an Online, a BrickAndMortar, and a CafeBranch
+		Bank rock = new Bank();
+		Online rockOnline = new Online();
+		BrickAndMortar rockBrickAndMortar = new BrickAndMortar();
+		CafeBranch rockCafeBranch = new CafeBranch();
 
 	}
 
